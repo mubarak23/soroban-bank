81c6151e6f4e33c63ece5ffff94f9cd359f644c1
mubarak23 <mubarakaminu340@gmail.com>
2024-09-12 16:13:54 +0100

    utilize bank account function inside main function

--- src/main.rs
+++ src/main.rs
@@ -58,7 +58,47 @@ impl BankAccount {
         let _ = reciver_bank_account.account_balance + amount;
         return Some(amount);
     }
+    fn bank_account_details (&self) -> (&str, &str, u32, &str) {
+        (&self.account_number, &self.account_name, self.account_balance, &self.bank_name)
+    }
 }
 fn main() {
+        // create account
+    let mut account1 = BankAccount::create_bank_account("9143456123".to_string(), "Mubarak23".to_string(), "Access Bank".to_string());
+    println!("New user Open account with ${:?}", account1.account_balance());
+    
+    // Depoosit amount and check balance
+   account1.deposit_into_account(5000);
+    println!("Balance after making a deposit  is ${}", account1.account_balance());
+
+    // Check account 1 details
+    let (account_number, account_name, account_balance, bank_name) = account1.bank_account_details();
+    println!("Account 1 details: Number: {}, Name: {}, Balance: ${}, Bank Name: ${}", account_number, account_name, account_balance, bank_name);
+
+    // Create new account
+    let mut account2 = BankAccount::create_bank_account("9876543201".to_string(), "Mubitech".to_string(), "Opay".to_string());
+    println!("New user Open account balance ${:?}", account2.account_balance());
+    println!("Account 2 details {:?}", account2.bank_account_details());
+
+
+    // Before transfer
+    println!("Before transfer:");
+    println!("Account 1 balance: ${}", account1.account_balance());
+    println!("Account 2 balance: ${}", account2.account_balance());
+     
+
+    // Perform transfer
+    let transfer_amount = 500;
+    match account1.transfer_from_account(transfer_amount, account2.clone()) {
+        Some(amount) => println!("Successfully transferred ${}", amount),
+        None => println!("Transfer failed"),
+    }
+
+    println!("After transfer:");
+    println!("Account 1 balance: ${}", account1.account_balance());
+    println!("Account 2 balance: ${}", account2.account_balance());
+
+
+
     println!("Hello, world!");
 }
